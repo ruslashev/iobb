8c5376f18234e9e330761870630338bcf8292913
Ruslan Akbashev <ruslashev@gmail.com>
2020-08-23 23:10:25 +0300

    fix abstract wm header

--- engine/wm/wm.hh
+++ engine/wm/wm.hh
@@ -1,26 +1,4 @@
 #pragma once
 
-#include "../macros.hh"
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
-#include "glfw/keys.hh"
-
-class wm
-{
-	GLFWwindow *_window;
-
-public:
-	wm();
-	void init(int w, int h, strlit title);
-	bool should_close();
-	void poll_events();
-	void swap_window();
-	bool key_down(key k);
-	bool mouse_key_down(mouse_key mk);
-	bool mouse_key_up(mouse_key mk);
-	void get_mouse_pos(float *x, float *y);
-	double get_time();
-	void set_title(const char *title);
-	~wm();
-};
+#include "glfw/wm.hh"
 
